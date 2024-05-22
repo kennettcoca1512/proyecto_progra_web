1b523be9436e523553f5ea9b9c14a9a5497805f9
Danny <20210630@aloe.ulima.edu.pe>
2024-05-21 23:45:23 -0500

    Merge login page

---
+++ my-react-app/src/routes/LoginPage.jsx
@@ -0,0 +1,19 @@
+import Header from "../components/Shares/Header/Header";
+import Footer from "../components/Shares/Footer/Footer";
+import Login from "../components/Login/Login";
+
+export default function LoginPage(){
+    
+    
+    return (
+        <>  
+            <Header/>
+            <Login/>
+            <Footer/>
+        </>
+        
+    );
+};
+
+
+
